991cae181bc7d92b7ec79fa3aec70f308e25b11e
henryobukohwo <henrypeermentor@gmail.com>
2024-05-09 14:53:35 +0100

    new codeline

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,22 +5,6 @@ class Program
     static void Main(string[] args)
     {
         //This is prep 2
-        int x = 5;
-        int y = 2;
-
-        if (x>y)
-        {
-           Console.WriteLine("Greater");
-
-        }
-        else if
-        {
-            Console.WriteLine("less");
-
-        }
-        else
-        {
-            Console.WriteLine("equal");
-        }
+        Console.WriteLine
     }
 }
