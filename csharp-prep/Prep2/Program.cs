824ebb085de7d863cc84be1c2125c2171a01df7d
henryobukohwo <henrypeermentor@gmail.com>
2024-05-09 14:53:10 +0100

    code added

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,6 +5,22 @@ class Program
     static void Main(string[] args)
     {
         //This is prep 2
-        Console.WriteLine("Hello Prep2 World!");
+        int x = 5;
+        int y = 2;
+
+        if (x>y)
+        {
+           Console.WriteLine("Greater");
+
+        }
+        else if
+        {
+            Console.WriteLine("less");
+
+        }
+        else
+        {
+            Console.WriteLine("equal");
+        }
     }
 }
