ac3281e3bec1c6d261f3255c3b7c48cc4cbd5a0c
henryobukohwo <henrypeermentor@gmail.com>
2024-05-11 03:39:30 +0100

    Code added

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,7 +4,63 @@ class Program
 {
     static void Main(string[] args)
     {
-        //This is prep 2
-        Console.WriteLine
+        /*This is prep 2. 
+        With the conditional statement*/
+
+        {
+            Console.Write("What is your grade percentage? ");
+            string userAnswer = Console.ReadLine();
+            int percentage = int.Parse(userAnswer);
+
+            string letter = "";
+
+            if (percentage >= 90)
+            {
+                letter = "A";
+
+            }
+            else if (percentage >= 80)
+            {
+                letter = "B";
+            }
+            else if (percentage >= 70)
+            {
+                letter = "c";
+            }
+            else if (percentage >= 60)
+            {
+                letter = "D";
+            }
+            else 
+            {
+                letter = "F";
+            }
+
+            {
+                Console.WriteLine($"Your grade is letter: {letter}");
+
+                if (percentage >= 70)
+                {
+                    Console.WriteLine(" Congrtulation! You passed");
+                } 
+                else
+                {
+                    Console.WriteLine("Best of luck next time");
+                }
+            }
+        
+
+
+
+        
+
+
+
+            
+        }
+        
+
+
+        
     }
 }
