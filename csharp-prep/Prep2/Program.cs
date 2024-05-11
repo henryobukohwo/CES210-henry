31d3c8b01e294b299834a7600ba66ecdda21c4a1
henryobukohwo <henrypeermentor@gmail.com>
2024-05-11 04:26:46 +0100

    retaining code to it normal state

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -13,16 +13,8 @@ class Program
             int percentage = int.Parse(userAnswer);
 
             string letter = "";
-            
-            if ( percentage >= 100)
-            {
-                letter = "A+"
-            }
-            else if (percentage >= 95)
-            {
-                letter = "A-"
-            }
-            else if (percentage >= 90)
+
+            if (percentage >= 90)
             {
                 letter = "A";
 
