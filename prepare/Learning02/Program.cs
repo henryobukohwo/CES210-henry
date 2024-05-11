c65d6eeb043d090da7c5482c7b5bee81911f62ab
henryobukohwo <henrypeermentor@gmail.com>
2024-05-11 03:43:09 +0100

    code added

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -6,4 +6,47 @@ class Program
     {
         Console.WriteLine("Hello Learning02 World!");
     }
-}
+}    /*This is prep 2. 
+        With the conditional statement*/
+
+        
+            Console.Write("What is your grade percentage? ");
+            string userAnswer = Console.ReadLine();
+            int percentage = int.Parse(userAnswer);
+
+            string letter = "";
+
+            if (percentage >= 90)
+            {
+                letter = "A";
+
+            }
+            else if (percentage >= 80)
+            {
+                letter = "B";
+            }
+            else if (percentage >= 70)
+            {
+                letter = "c";
+            }
+            else if (percentage >= 60)
+            {
+                letter = "D";
+            }
+            else 
+            {
+                letter = "F";
+            }
+
+            {
+                Console.WriteLine($"Your grade is letter: {letter}");
+
+                if (percentage >= 70)
+                {
+                    Console.WriteLine(" Congrtulation! You passed");
+                } 
+                else
+                {
+                    Console.WriteLine("Best of luck next time");
+                }
+            }
